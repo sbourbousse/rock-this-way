f05bbf0066df954c01090e53b18222dae5bf2715
sbourbousse <sylvain.bourbousse@epitech.eu>
2021-06-21 15:00:44 +0200

    Change port and ts compiler for heroku

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const user = require('./routes/users')
 const bodyParser = require('body-parser');
 const app = express()
-const port = 3000
+const port = 80
 
 
 app.use(bodyParser.json() );       // to support JSON-encoded bodies
