b14859146121e4a0544b6624b82550c9c5cde89d
RayanMediouni <rayan.mediouni@epitech.eu>
2021-07-05 15:46:40 +0200

    send mail using ethereal

--- src/controllers/users/createUser.ts
+++ src/controllers/users/createUser.ts
@@ -1,5 +1,6 @@
 import { User } from '../../Class/User';
 import { Role } from '../../Class/Role';
+const nodemailer = require("nodemailer");
 
 
 var jwt = require("jsonwebtoken");
@@ -16,7 +17,7 @@ module.exports = function (req: any, res: any) {
     if(check.valid == false) {
         res.status(400).json(check);
     } else {
-        user.save((err: string) => {
+        user.save(async (err: string) => {
             if (err) {
                 res.status(500).json({
                     valid: false,
@@ -24,7 +25,28 @@ module.exports = function (req: any, res: any) {
                 })
                 throw err;
             }
-    
+            const transporter = nodemailer.createTransport({
+                host: 'smtp.ethereal.email',
+                port: 587,
+                auth: {
+                    user: 'jadon.nicolas18@ethereal.email',
+                    pass: 'TfSNHMer6w3txnhpnA'
+                }
+            });
+            let info = await transporter.sendMail({
+                from: '"Rock this way" <contact@rockthisway.com>', // sender address
+                to: user.email, // list of receivers
+                subject: "Bienvenue a Rock This Way ! ✔", // Subject line
+                text: "Hello world?", // plain text body
+                html: "<b>Hello world?</b>", // html body
+              });
+            
+              console.log("Message sent: %s", info.messageId);
+              
+              // Preview only available when sending through an Ethereal account
+              console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+             
+
             if (req.body.roles) {
                 Role.toMongooseSchema().find({
                         name: { $in: req.body.roles }
